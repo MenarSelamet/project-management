a191e4ee700030157dc49b53261df7585e0ffa49
Menar Selamet <menarselamet007@gmail.com>
2025-03-09 06:05:49 +0300

    Styling navbar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Search} from "lucide-react";
+import { Search, Settings } from "lucide-react";
+import Link from "next/link";
 
 const index = () => {
   return (
@@ -7,10 +8,22 @@ const index = () => {
       {/* {search} */}
       <div className="flex items-center gap-8">
         <div className="relative flex h-min w-[200px]">
-            <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white"/>
-            <input className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white" type="search" placeholder="Search..." />
-
+          <Search className="absolute top-1/2 left-[4px] mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
+          <input
+            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
+            type="search"
+            placeholder="Search..."
+          />
         </div>
+      </div>
+      <div className="flex items-center">
+        <Link
+          href="/settings"
+          className="h-min w-min rounded p-2 hover:bg-gray-100"
+        >
+          <Settings className="cursor-point h-6 w-6 dark:text-white" />
+        </Link>
+        <div className="mr-5 ml-2 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
       </div>
     </div>
   );
