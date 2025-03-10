df1f13593482d7c80e8ef469f62ab4a0f9060c1e
Menar Selamet <menarselamet007@gmail.com>
2025-03-10 06:16:11 +0300

    Sidebarlinks added to the sidebar

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
+import Link from "next/link";
 import React, { useState } from "react";
 import Image from "next/image";
-import { LockIcon } from "lucide-react";
+import { Home, LockIcon, LucideIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { Icon } from "lucide-react";
 
 const index = () => {
-  const [showProjects, setShowProjects] = useState(true);
-  const [showPriority, useStatePriority] = useState(true);
+  // const [showProjects, setShowProjects] = useState(true);
+  // const [showPriority, useStatePriority] = useState(true);
 
   const sideBarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
 
@@ -32,8 +37,42 @@ const index = () => {
           </div>
         </div>
         {/* Narvbar Links */}
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+        </nav>
       </div>
     </div>
+  );
+};
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+}
+
+const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
+          isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+        } justify-start px-8 py-3`}
+      >
+        {isActive && (
+          <div className="absolute top-0 left-0 h-[100%] w-[5px] bg-blue-200" />
+        )}
+
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
 
