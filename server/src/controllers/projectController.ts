409f84283be48421b593c8542a3309f2ccb2c601
Menar Selamet <menarselamet007@gmail.com>
2025-03-11 06:59:15 +0300

    Project Route complete

--- server/src/controllers/projectController.ts
+++ server/src/controllers/projectController.ts
@@ -3,14 +3,14 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const getProjects = async(
-    req: Request,
-    res: Response
-): Promise<void>=>{
-    try {
-        const projects = await prisma.project.findMany();
-        res.json(projects);
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving projects"})
-    }
-}
+export const getProjects = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const projects = await prisma.project.findMany();
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving projects" });
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,29 +5,29 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
-// Route Imports 
+//Route Imports
+import projectRoutes from "./routes/projectRoutes";
 
-
-// Configurations
+//Configurations
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+//Routes
+app.get("/", (req, res) => {
+  res.send("This is home route");
+});
 
-// Routes
-app.get('/', (req, res) => {
-    res.send("This is home route");
-})
-
+app.use("/projects", projectRoutes);
 
 //Server
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
