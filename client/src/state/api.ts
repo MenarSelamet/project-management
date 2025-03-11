d7cb1ef4105d4a574d9b268a8e5345b48ffb182a
Menar Selamet <menarselamet007@gmail.com>
2025-03-11 23:54:34 +0300

    Creating api functions to connect to backend with redux

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -63,8 +63,21 @@ export interface Task {
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
+  tagTypes: ["Projects"],
+  endpoints: (build) => ({
+    getProjects: build.query<Project[], void>({
+      query: () => "projects",
+      providesTags: ["Projects"],
+    }),
+    createProject: build.mutation<Project, Partial<Project>>({
+        query: (project) => ({
+            url: "projects",
+            method: "POST",
+            body: project
+        }),
+        invalidatesTags: ["Projects"],
+    }),
+  }),
 });
 
-export const {} = api;
+export const { useGetProjectsQuery, useCreateProjectMutation } = api;
