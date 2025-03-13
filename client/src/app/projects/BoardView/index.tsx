a1e87860925e7220a1986819f44ccd636401a469
Menar Selamet <menarselamet007@gmail.com>
2025-03-13 03:05:00 +0300

    Adding number of comments to tasks

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Task as TaskType } from "@/state/api";
-import { EllipsisVertical, Plus } from "lucide-react";
+import { EllipsisVertical, MessageSquareMore, Plus } from "lucide-react";
 import { format } from "date-fns";
 import Image from "next/image";
 import { Span } from "next/dist/trace";
@@ -234,6 +234,22 @@ const Task = ({ task }: TaskProps) => {
                   className="dark:border-dark-secondary h-8 w-8 rounded-full border-2 border-white object-cover"
                 />
               )}
+              {task.author && (
+                <Image
+                  key={task.author.userId}
+                  src={`/${task.author.profilePictureUrl!}`}
+                  alt={task.author.username}
+                  width={30}
+                  height={30}
+                  className="oboject-cover dark:border-dark-secondary h-8 w-8 rounded-full border-2 border-white"
+                />
+              )}
+            </div>
+            <div className="flex items-center text-gray-500 dark:text-neutral-500">
+              <MessageSquareMore size={20} />
+              <span className="ml-1 text-sm dark:text-neutral-400">
+                {numberOfComments}
+              </span>
             </div>
           </div>
         </div>
