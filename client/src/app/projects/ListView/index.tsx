26eccc7eb865d4ac0976caa4ebc85dcb21acd654
Menar Selamet <menarselamet007@gmail.com>
2025-03-13 03:43:54 +0300

    Adding attachments to the list page

--- client/src/app/projects/ListView/index.tsx
+++ client/src/app/projects/ListView/index.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
-import { useGetTasksQuery } from "@/state/api";
+import { Task, useGetTasksQuery } from "@/state/api";
 import React from "react";
+import TaskCard from "@/components/TaskCard";
 
 type Props = {
   id: string;
@@ -21,6 +22,9 @@ const ListView = ({ id, setIsModalNewTaskOpen }: Props) => {
     <div className="px-4 pb-8 xl:px-6">
       <div className="pt-5">
         <Header name="List" />
+      </div>
+      <div className="gird gid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6">
+        {tasks?.map((task: Task) => <TaskCard key={task.id} task={task} />)}
       </div>
     </div>
   );
