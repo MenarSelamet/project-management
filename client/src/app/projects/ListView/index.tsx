c5d28107b5e4a13a721512d695c0a35051251f14
Menar Selamet <menarselamet007@gmail.com>
2025-03-13 03:35:09 +0300

    Creating a list page

---
+++ client/src/app/projects/ListView/index.tsx
@@ -0,0 +1,29 @@
+import Header from "@/components/Header";
+import { useGetTasksQuery } from "@/state/api";
+import React from "react";
+
+type Props = {
+  id: string;
+  setIsModalNewTaskOpen: (isOpen: boolean) => void;
+};
+
+const ListView = ({ id, setIsModalNewTaskOpen }: Props) => {
+  const {
+    data: tasks,
+    error,
+    isLoading,
+  } = useGetTasksQuery({ projectId: Number(id) });
+
+  if (isLoading) return <div>Loading ...</div>;
+  if (error) return <div>An error occured</div>;
+
+  return (
+    <div className="px-4 pb-8 xl:px-6">
+      <div className="pt-5">
+        <Header name="List" />
+      </div>
+    </div>
+  );
+};
+
+export default ListView;
