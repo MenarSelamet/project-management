740b49e276011b4a192c76b59e9941ea68b4cd2a
Menar Selamet <menarselamet007@gmail.com>
2025-03-13 04:32:34 +0300

    Creating a timeline table with the gantt taks library

--- client/src/app/projects/TimelineView/index.tsx
+++ client/src/app/projects/TimelineView/index.tsx
@@ -1,7 +1,7 @@
 import { useAppSelector } from "@/app/redux";
 import { useGetTasksQuery } from "@/state/api";
 import React, { useMemo, useState } from "react";
-import { DisplayOption, ViewMode } from "gantt-task-react";
+import { DisplayOption, Gantt, ViewMode } from "gantt-task-react";
 
 type Props = {
   id: string;
@@ -33,7 +33,7 @@ const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
         end: new Date(task.dueDate as string),
         name: task.title,
         id: `Task-${task.id}`,
-        type: "task" as "TaskType",
+        type: "task" as TaskTypeItems,
         progress: task.points ? (task.points / 10) * 100 : 0,
         isDisabled: false,
       })) || []
@@ -57,7 +57,7 @@ const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
         </h1>
         <div className="relative inline-block w-64">
           <select
-            className="focus:shadow-outline dark:border-dark-secondary dark:bg-dark-secondary block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none dark:text-white"
+            className="focus:shadow-outline dark:border-dark-secondary dark:bg-dark-secondary block w-full appearance-none rounded border border-gray-400 bg-neutral-200 px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none dark:text-white"
             value={displayOptions.viewMode}
             onChange={handleViewModeChange}
           >
@@ -65,6 +65,27 @@ const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
             <option value={ViewMode.Week}>Week</option>
             <option value={ViewMode.Month}>Month</option>
           </select>
+        </div>
+      </div>
+
+      <div className="dark:bg-dark-secondary overflow-hidden rounded-md bg-white shadow dark:text-white">
+        <div className="timeline">
+          <Gantt
+            tasks={ganttTasks}
+            {...displayOptions}
+            columnWidth={displayOptions.viewMode === ViewMode.Month ? 150 : 100}
+            listCellWidth="100px"
+            barBackgroundColor={isDarkMode ? "#101214" : "#aeb8c2"}
+            barBackgroundSelectedColor={isDarkMode ? "#000" : "#9ba1e6"}
+          />
+        </div>
+        <div className="px-4 pt-1 pb-5">
+          <button
+            className="bg-blue-primary flex items-center rounded px-3 py-2 text-white hover:bg-blue-600"
+            onClick={() => setIsModalNewTaskOpen(true)}
+          >
+            Add new Task
+          </button>
         </div>
       </div>
     </div>
