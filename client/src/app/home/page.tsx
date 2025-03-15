7e3f80448acbcd784f931a09d9acfd089573eff5
Menar Selamet <menarselamet007@gmail.com>
2025-03-16 02:10:23 +0300

    Creating the homepage

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,25 @@
+import { useGetProjectsQuery, useGetTasksQuery } from "@/state/api";
+import React from "react";
+import { useAppSelector } from "../redux";
+
+const HomePage = () => {
+  const {
+    data: tasks,
+    isLoading: tasksLoading,
+    isError: tasksError,
+  } = useGetTasksQuery({
+    projectId: parseInt("1"),
+  });
+
+  const { data: projects, isLoading: isProjectsLoading } =
+    useGetProjectsQuery();
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  if (tasksLoading || isProjectsLoading) return <div>Loading...</div>;
+  if (tasksError || tasks || !projects) return <div>Error fetching data</div>;
+
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
