af5090daaaa8c39918b6530ba9a843f151bbb0d8
Menar Selamet <menarselamet007@gmail.com>
2025-03-15 08:04:41 +0300

    Connecting api for users

---
+++ client/src/app/teams/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "@/components/Header";
+
+// type Props = {}
+
+const Teams = () => {
+  return (
+    <div>
+      <Header name="Teams" />
+      Teamsuuu pageuuuu
+    </div>
+  );
+};
+
+export default Teams;
