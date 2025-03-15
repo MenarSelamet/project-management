da9925188102d3ff15ac037d94f221354a4a9176
Menar Selamet <menarselamet007@gmail.com>
2025-03-15 08:18:42 +0300

    Adding users to user page

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -1,14 +1,64 @@
+"use client";
+
 import React from "react";
 import Header from "@/components/Header";
+import { useGetUsersQuery } from "@/state/api";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
+import { useAppSelector } from "../redux";
 
-// type Props = {}
+const columns: GridColDef[] = [
+  {
+    field: "userId",
+    headerName: "ID",
+    width: 100,
+  },
+  {
+    field: "username",
+    headerName: "Username",
+    width: 150,
+  },
+  {
+    field: "profilePictureUrl",
+    headerName: "Profile Picture",
+    width: 100,
+    renderCell: (params) => (
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="h-9 w-9">
+          <Image
+            src={`/${params.value}`}
+            alt={params.row.username}
+            width={100}
+            height={50}
+            className="h-full rounded-full object-cover"
+          />
+        </div>
+      </div>
+    ),
+  },
+];
 
 const Users = () => {
-  
+  const { data: users, isLoading, isError } = useGetUsersQuery();
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError || !users) return <div>Error fetching Users...</div>;
+
   return (
-    <div>
+    <div className="flex w-full flex-col p-8">
       <Header name="Users" />
-      Usersss pageuuuuu
+      <div style={{ height: 650, width: "100%" }}>
+        <DataGrid
+          rows={users || []}
+          columns={columns}
+          getRowId={(row) => row.userId}
+          pagination
+          className={dataGridClassNames}
+          sx={dataGridSxStyles(isDarkMode)}
+        />
+      </div>
     </div>
   );
 };
