f84fc848ecf66c1619876c176627b6bcb0b9d6b8
Menar Selamet <menarselamet007@gmail.com>
2025-03-15 07:13:25 +0300

    Search functionality complete

---
+++ client/src/components/UserCard/index.tsx
@@ -0,0 +1,28 @@
+import { User } from "@/state/api";
+import Image from "next/image";
+
+type Props = {
+  user: User;
+};
+
+const TaskCard = ({ user }: Props) => {
+  return (
+    <div className="flex items-center rounded border p-4 shadow">
+      {user.profilePictureUrl && (
+        <Image
+          src={`/p1.jpeg`}
+          alt="Profile Picture"
+          width={32}
+          height={32}
+          className="rounded-full"
+        />
+      )}
+      <div>
+        <h3>{user.username}</h3>
+        <p>{user.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
