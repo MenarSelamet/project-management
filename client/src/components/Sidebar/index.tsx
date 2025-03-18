fd5b9f19104d3f32c232d26a87942f8701d397e1
Menar Selamet <menarselamet007@gmail.com>
2025-03-19 02:46:40 +0300

    Fixing images bug

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -62,7 +62,7 @@ const index = () => {
         {/* Team */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
           <Image
-            src="https:://pms3images-ms.s3.us-east-1.amazonaws.com/logo.jpg"
+            src="https://pms3images-ms.s3.us-east-1.amazonaws.com/logo.jpg"
             alt="Logo"
             width={40}
             height={40}
