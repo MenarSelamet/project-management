f6dc0aef3779ac4c6ccb7efa94bdd5e05169338e
Menar Selamet <menarselamet007@gmail.com>
2025-03-19 08:56:25 +0300

    Authentication set up

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure({
   Auth: {
@@ -11,11 +13,40 @@ Amplify.configure({
   },
 });
 
+const formFields = {
+  signUp: {
+    username: {
+      order: 1,
+      placeholder: "Choose a username",
+      label: "Username",
+      inputProps: { required: true },
+    },
+    email: {
+      order: 2,
+      placeholder: "Enter your email address",
+      label: "email",
+      inputProps: { type: "email", required: true },
+    },
+    password: {
+      order: 3,
+      placeholder: "Enter your password",
+      label: "Password",
+      inputProps: { type: "password", required: true },
+    },
+    confirm_password: {
+      order: 4,
+      placeholder: "Confirm your password",
+      label: "Confirm Password",
+      inputProps: { type: "password", required: true },
+    },
+  },
+};
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const AuthProvider = ({ children }: any) => {
   return (
     <div className="mt-5">
-      <Authenticator>
+      <Authenticator formFields={formFields}>
         {({ user }: any) =>
           user ? (
             <div>{children}</div>
